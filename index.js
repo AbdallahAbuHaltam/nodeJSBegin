d7bc44c2888f9c40edf2b9faa06f30faf85848e2
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-11-29 19:55:38 +0300

    Routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs=require('fs');
 const http=require('http');
+const url=require('url');
 
 ////FILES
 
@@ -33,7 +34,19 @@ const http=require('http');
 ///////
 //Server
 const server=http.createServer((req,res)=>{
-    res.end('Hello From the Server!');
+    const pathName=req.url;
+    if(pathName==='/'||pathName==='/overview'){
+        res.end('This is the OverView');
+    }
+    else if(pathName==='/product'){
+        res.end('This is the Product');
+    }else{
+        res.writeHead(404,{
+            'Content-Type':'text/html',
+            'my-own-header':'hello-world',
+        });
+        res.end('<h1>Page Not Found</h1>');
+    }
 });
 server.listen(8000,'127.0.0.1',()=>{
     console.log('Listening to Request on Port 8000');
