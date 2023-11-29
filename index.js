1cb12a3f1bca5fcf19a5323c3fe22512ac0f50d9
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-11-29 19:34:33 +0300

    Creating a simple Web Server

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const fs=require('fs');
+const http=require('http');
+
+////FILES
 
 //Blocking synchronous way
 // const textIn=fs.readFileSync('./txt/input.txt','utf-8');
@@ -10,19 +13,28 @@ const fs=require('fs');
 // console.log('File is Written');
 
 //Non-Blocking Asynchronous way
-fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
-    if(err){
-        return console.error(err);
-    }
-    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
-        console.log(data2);
-        fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
-            console.log(data3);
+// fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
+//     if(err){
+//         return console.error(err);
+//     }
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
+//             console.log(data3);
 
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
-                console.log('Your file has been Written!');
-            })
-        });
-    });
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
+//                 console.log('Your file has been Written!');
+//             })
+//         });
+//     });
+// });
+// console.log('Will Read File ....');
+
+///////
+//Server
+const server=http.createServer((req,res)=>{
+    res.end('Hello From the Server!');
 });
-console.log('Will Read File ....');
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('Listening to Request on Port 8000');
+});
