027028b99e98ef7da9f8b7b33e9eee2a93cbe181
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-11-29 17:49:37 +0300

    Reading And Writing Files

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const hello='Hello World';
-console.log(hello);
+const fs=require('fs');
+
+const textIn=fs.readFileSync('./txt/input.txt','utf-8');
+console.log(textIn);
+
+const textOut=`This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log('File is Written');
