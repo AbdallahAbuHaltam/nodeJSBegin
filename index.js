13b07a188ba313a34e9704ee564a5f97edc0e732
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-12-01 16:28:14 +0300

    Building a very Simple API

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ const url=require('url');
 
 ///////
 //Server
+const data=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj=JSON.parse(data);
+    
 const server=http.createServer((req,res)=>{
     const pathName=req.url;
     if(pathName==='/'||pathName==='/overview'){
@@ -40,7 +43,13 @@ const server=http.createServer((req,res)=>{
     }
     else if(pathName==='/product'){
         res.end('This is the Product');
-    }else{
+    }else if(pathName==='/api'){
+        res.writeHead(200,{
+            'Content-Type':'application/json',
+        });
+        res.end(data);
+        
+    } else{
         res.writeHead(404,{
             'Content-Type':'text/html',
             'my-own-header':'hello-world',
